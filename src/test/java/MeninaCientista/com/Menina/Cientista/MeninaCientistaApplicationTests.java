8c65b33ff80ca29e6bad027c5d50c5c95612f20c
Emerson Reis <emersonreisdacosta@gmail.com>
2021-01-27 23:09:48 -0400

    projeto criado

---
+++ src/test/java/MeninaCientista/com/Menina/Cientista/MeninaCientistaApplicationTests.java
@@ -0,0 +1,14 @@
+
+package MeninaCientista.com.Menina.Cientista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeninaCientistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
