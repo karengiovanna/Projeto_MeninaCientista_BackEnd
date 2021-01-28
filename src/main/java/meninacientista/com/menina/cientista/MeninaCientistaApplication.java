ecf4667363fda30e3e69cc3931730b8c71cc4907
Emerson Reis <emersonreisdacosta@gmail.com>
2021-01-27 23:38:59 -0400

    corrigido duplicidade de codigo

---
+++ src/main/java/meninacientista/com/menina/cientista/MeninaCientistaApplication.java
@@ -0,0 +1,13 @@
+package meninacientista.com.menina.cientista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeninaCientistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeninaCientistaApplication.class, args);
+	}
+
+}
